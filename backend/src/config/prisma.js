c5666a15eff251b2d0a7721989e805ed2a4f0b51
Ashish Rajput <rajputashu2018@gmail.com>
2026-08-28 15:58:03 +0530

    Fix Prisma client for Vercel

--- backend/src/config/prisma.js
+++ backend/src/config/prisma.js
@@ -1,6 +1,6 @@
 import "dotenv/config";
 
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated/prisma/client.js";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 
 const adapter = new PrismaMariaDb({
